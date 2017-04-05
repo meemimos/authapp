b02fe0a083fcda8415f5b5e6e5d306863d37b04c
meemimos <sayabcz@hotmail.com>
2017-04-05 15:57:29 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//bringing on our all dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+// Connect To database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database ' + config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: ' + err);
+});
+
+
+// Initializing our app at express
+const app = express();
+
+// bringing users from the routes folder
+const users = require('./routes/users');
+
+// port number
+const port = 3000;
+
+//CORS Middleware
+// providing a Connect/Express middleware that can be used to enable CORS
+app.use(cors());
+
+// Set static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+});
